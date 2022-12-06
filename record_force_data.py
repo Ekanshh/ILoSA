3f7aea1fddc11f0b3b8e107d30e3bccfedd0e8e3
Ekanshh <ekanshh.sharma@gmail.com>
2022-12-06 17:10:03 +0100

    Updated record_force_torque script: - Receive e_start trigger for recording and saving wrench data - Added a WrenchClassifier for prediction of contact side

--- record_force_data.py
+++ record_force_data.py
@@ -5,6 +5,33 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import WrenchStamped
 
+DATA_COLLECTION = False
+
+import torch
+import numpy as np
+
+class WrenchClassifier:
+
+    def __init__(self) -> None:
+        self.model_path = "/home/urbant/ilosa_ws/src/ILoSA/ft_classifier/Models/panda_ft_classifier"
+        self.load()
+    
+    def load(self, ):
+        self.model = torch.load(self.model_path)
+        self.model.eval()
+    
+    def inference(self, X):
+        X = np.array(X, dtype=np.float32).T[None, ]
+        print(f"Shape of input data: {X.shape}")
+        # print(X)
+        X = torch.tensor(X)
+        _, prediction = torch.max(self.model(X), 1)
+        if prediction == 0:
+            side = "left"
+        else:
+            side = "right"
+        print(f"Contact side prediction: {prediction, side}")
+        return side 
 
 class RecordWrenchData:
 
@@ -15,6 +42,7 @@ class RecordWrenchData:
         self.dump_data = []
         self.wrench_data1 = []
         self.dump_data1 = []
+        self.side_pub = rospy.Publisher('/wrench_inference/side', String, queue_size=10)
         rospy.Subscriber("/record_wrench_data/event_in", String, self.event_in_callback)
         rospy.Subscriber("/force_torque_ext", WrenchStamped, self.record_wrench_callback)
         rospy.Subscriber("/franka_state_controller/F_ext", WrenchStamped, self.record_wrench_callback_1)
@@ -22,7 +50,12 @@ class RecordWrenchData:
 
     def event_in_callback(self, msg):
         rospy.loginfo("Waiting for event call")
-        if msg.data == 'e_start_right':
+
+        if msg.data == 'e_start':
+            rospy.loginfo("Trigger received to record wrench data for inference....")
+            self.e_start = 'start'
+
+        elif msg.data == 'e_start_right':
             rospy.loginfo("e_start | right")
             self.e_start= 'start'
             self.side=msg.data[8:]
@@ -36,17 +69,27 @@ class RecordWrenchData:
             rospy.loginfo("e_stop")
             self.e_start= 'stop'
             rospy.loginfo("Stopping data")
-            self.dump_data.append((self.side, self.wrench_data))
-            self.dump_data1.append((self.side, self.wrench_data1))
-            self.side = None
-            self.wrench_data = []
-            self.wrench_data1 = []
+            if DATA_COLLECTION:
+                self.dump_data.append((self.side, self.wrench_data))
+                self.dump_data1.append((self.side, self.wrench_data1))
+                self.side = None
+                self.wrench_data = []
+                self.wrench_data1 = []
+            else:
+                rospy.loginfo("Inference started...")
+                classifier = WrenchClassifier()
+                self.side = classifier.inference(self.wrench_data1)
+                self.side_pub.publish(self.side)
+                self.dump_data1.append((self.side, self.wrench_data1))
+                self.side = None
+                self.wrench_data = []
+                self.wrench_data1 = []
 
         elif msg.data == 'e_save':
-            with open('record_wrench_data_left.pickle', 'ab+') as f:
+            with open('record_wrench_data_inference.pickle', 'ab+') as f:
                 pickle.dump(self.dump_data, f)
                 self.dump_data = []
-            with open('record_wrench_data1_left.pickle', 'ab+') as f:
+            with open('record_wrench_data1_inference.pickle', 'ab+') as f:
                 pickle.dump(self.dump_data1, f)
                 self.dump_data1 = []
         else:
